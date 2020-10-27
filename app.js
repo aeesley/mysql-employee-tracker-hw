585ae02133d715f9cc1fd480dca59d3092410802
Alex Eesley <alexandra.eesley@gmail.com>
2020-10-26 20:39:46 -0500

    added startQuestions function to populate user selection questions. This is working

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
-const inquirer = require("inquirer");
-const mysql = require("mysql");
-const fs = require("fs");
-const express = require("express");
+// Requiring mysql, inquirer and fs
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "employee_db"
-});
+// // Creating the mysql connection
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "password",
+//   database: "employee_db"
+// });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  connection.end();
-});
+// // mysql callback function
+// connection.connect(function(err) {
+//     if (err) throw err;
+//     console.log("connected as id " + connection.threadId);
+//     connection.end();
+//   });
+
+// Inquirer function to start initial questions
+function startQuestions(){
+    inquirer.prompt({
+        message: "What would you like to do?",
+        type: "list",
+        choices: ["View All Employees", "View All Employees By Department", "View All Employees By Manager", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager", "QUIT PROGRAM"],
+        name: "userChoice"
+    }).then()
+}
+
+startQuestions();
+
+
+
+
+
--- employeeSeeds.sql
+++ employeeSeeds.sql
@@ -1,13 +1,30 @@
+/* creating database parameters and sample data for mysql databases*/
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
+/* designating which database to use*/
 USE employee_db;
 
+/* employee table*/
 CREATE TABLE employee (
   id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  first_name VARCHAR(255) NULL,
-  last_name VARCHAR(255) NULL,
-  role_id INT(100),
-  manager_id INT(100 NULL),
+  first_name VARCHAR(30) NULL,
+  last_name VARCHAR(30) NULL,
+  role_id INT(10),
+  manager_id INT(10) NULL,
+);
 
+/* role table*/
+CREATE TABLE role (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  title VARCHAR(255) NULL,
+  salary VARCHAR(255) NULL,
+  department_id INT(100),
+  manager_id INT(100 NULL),
+);
+
+/* department table table*/
+CREATE TABLE department (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  department_name VARCHAR(30) NULL,
 );
