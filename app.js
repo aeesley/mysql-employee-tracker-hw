2aba00ad7e29c1b306fcf91429a0db1cce2de505
Alex Eesley <alexandra.eesley@gmail.com>
2020-10-27 20:56:15 -0500

    adding comments to explain the three add functions

--- app.js
+++ app.js
@@ -83,8 +83,10 @@ function viewAllEmployeesByManager(){
     // Copy by department logic and update here
 }
 
+// Function to add employee
 function addEmployee(){
     console.log('time to add an employee!!')
+    // using inquirer prompt to ask user input questions to store
     inquirer.prompt([
         {
             message: "What is the employee's first name?",
@@ -108,15 +110,17 @@ function addEmployee(){
         },
     ]).then(answers => {
         console.log("Did we add a new employee??", answers);
-
+        // adding mysql syntax to insert the new employee per the user input to the employee table
         connection.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) values (?, ?, ?, ?)", [answers.first_name, answers.last_name, answers.role_id, answers.manager_id], function(err, res) {
             console.log('err , res ?? did we add a new employee??!!', err, res)
         })
     })
 }
 
+// adding a new role function
 function addRole() {
     console.log('time to add a role')
+    // using inquirer to ask user input questions
     inquirer.prompt([
         {
             message: "What type of role would you like to add?",
@@ -135,21 +139,23 @@ function addRole() {
         },
     ]).then(answers => {
         console.log("Did we add a new role?", answers);
-
+        // using mysql syntax to insert new role designated by user input to the role table
         connection.query("INSERT INTO role (title, salary, department_id) values (?, ?, ?)", [answers.title, answers.salary, answers.department_id], function(err, res) {
             console.log('err , res ?? did we make a new dept!!!', err, res)
         })
     })
 }
-
+// function to add new department
 function addDepartment() {
     console.log('time to add dpet!!')
+    // using inquirer prompt to ask user input question for new department
     inquirer.prompt({
         message: "What department would you like to add?",
         type: "input",
         name: "newDepartment"
     }).then(answers => {
         console.log("Did adding new dept work?", answers);
+        // using mysql syntax to insert new department designated by user to department tables
         connection.query("INSERT INTO department (department_name) values (?)", [answers.newDepartment], function(err, res) {
             console.log('err , res ?? did we make a new dept!!!', err, res)
         })
